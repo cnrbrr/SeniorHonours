4751449281817c574e1b3d8621b1a380b558ab84
Conor <scb26@st-andrews.ac.uk>
2017-01-26 17:32:11 +0000

    finished dev for now

--- dev/js/register.js
+++ dev/js/register.js
@@ -3,12 +3,14 @@ $( document ).ready(function() {
 	$textInput.appendTo($('#text1'));
 	$textInput = $('<input type="text" style="width: 30%"id="sname" placeholder="Please Enter your last name here">');
 	$textInput.appendTo($('#text2'));
-	var $textInput3 = $('<input type="text" style="width: 30%"id="emailAdd" placeholder="Please Enter your email here">');
-	$textInput3.appendTo($('#text3'));
-	var $textInput4 = $('<input type="password" style="width: 30%"id="initPass" placeholder="Please Enter your password here">');
-	$textInput4.appendTo($('#pass1'));
-	var $textInput5 = $('<input type="password" style="width: 30%"id="sname" placeholder="Please repeat your password">');
-	$textInput5.appendTo($('#pass2'));
+	$textInput = $('<input type="text" style="width: 30%"id="emailAdd" placeholder="Please Enter your email here">');
+	$textInput.appendTo($('#text3'));
+	$textInput = $('<input type="password" style="width: 30%"id="initPass" placeholder="Please Enter your password here">');
+	$textInput.appendTo($('#pass1'));
+	$textInput = $('<input type="password" style="width: 30%"id="sPass" placeholder="Please repeat your password">');
+	$textInput.appendTo($('#pass2'));
+	var $input1 = $('<input type="button" class="btn btn-primary text-center" value="Register" id="regBtn"/>');
+    $input1.appendTo($("#btnSub"));
 });
 
 $( document ).ready(function() {
@@ -22,7 +24,83 @@ $( document ).ready(function() {
 	});
 });
 
+$( document ).ready(function() {
+	$( '#regBtn' ).click(function(){
+		var $first = $( '#fname' ).val();
+		var $second = $( '#sname' ).val();
+		var $email = $( '#emailAdd' ).val();
+		var $passFirst = $( '#initPass' ).val();
+		var $level = $( '#rangeVal' ).val();
+		var $passSecond = $( '#sPass' ).val();
+		var $DOB = $( '#bday' ).val();
+		var $gender = getGender();
+		if(nameCheck($first) === false){
+			alert("Please enter a valid first name!");
+			return;
+		}
+		if(nameCheck($second) === false){
+			alert("Please enter a valid last name!");
+			return;
+		}
+		if(isEmail($email) === false){
+			alert("Please enter a valid email address!");
+			return;
+		}
+		if($passFirst !== $passSecond){
+			alert("Please ensure both passwords are matching!");
+			return;
+		}
+		if(checkDate($DOB) ===false){
+			alert("Please enter a valid Date of Birth!");
+			return;
+		}
+		if($gender === 4){
+			alert("Please select your gender");
+			return;
+		}
+		alert($first + " " + $second + " " + $email + " " + $passFirst + " " + $passSecond + " " + $level + " " + $DOB);
+	});
+});
+
 function isEmail(email) {
   var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
   return regex.test(email);
+}
+
+function checkDate(date){
+	if(date !== "" && date !== null && date !== " "){
+		var regEx = /^\d{4}-\d{2}-\d{2}$/;
+  		return date.match(regEx);
+	}else{
+		return false;
+	}
+}
+function getGender(){
+	var $male = $('input[name=optradio1]:checked').val();
+	var $female = $('input[name=optradio2]:checked').val();
+	var $other = $('input[name=optradio3]:checked').val();
+	if($male == "on"){
+		return 1;
+	}
+
+	if($female == "on"){
+		return 2;
+	}
+
+	if($other == "on"){
+		return 3;
+	}
+	if($male !== "on" && $female !== "on" && $other !== "on"){
+		alert("4");
+		return 4;
+	}
+}
+
+function nameCheck(fname){
+	if(fname !== null && fname !== " " && fname !== ""){//ensures not null
+		var tester = /^[A-Za-z ]+$/.test(fname);
+		return tester;
+	}else{
+		return false;
+	}
 }
