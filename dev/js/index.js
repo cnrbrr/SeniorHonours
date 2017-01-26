d4d4d01d51f6a069b28d6bd27210e58e6c2ee76f
Conor <scb26@st-andrews.ac.uk>
2017-01-26 17:47:33 +0000

    Added login features

--- dev/js/index.js
+++ dev/js/index.js
@@ -3,7 +3,7 @@
 $( document ).ready(function() {
 	var $heading = $('<h1>Senior Honours</h1>');
 	$heading.appendTo($("#titleHead"));
-	var $input1 = $('<input type="button" class="btn btn-primary text-center" value="login" id="logButton"/>');
+	var $input1 = $('<input type="button" class="btn btn-primary text-center" value="login" id="logButton" data-toggle="modal" data-target="#loginModal"/>');
     $input1.appendTo($("#btn1"));
     var $input2 = $('<input type="button" class="btn btn-primary text-center" value="register" id="regButton"/>');
     $input2.appendTo($("#btn2"));
@@ -16,8 +16,23 @@ $( "#regButton" ).click(function(){
 });
 
 $( document ).ready(function() {
-$( "#logButton" ).click(function(){
+$( "#modalLogin1" ).click(function(){
+	//change to other screen where tasks will be
+	var $email = $('#emailCheck').val();
+	var $pass = $('#passCheck').val();
+	if($email === null || $email === "" || $email === " "){
+		alert("Please Enter a valid email address!");
+		return;
+	}
+	if($pass === null || $pass === "" || $pass === " "){
+		alert("Please Enter a valid password!");
+		return;
+	}
+	alert("Logging in");
+});
+});
 
-alert("h");
-});
-});
+function isEmail(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email) != null;
+}
